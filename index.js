bf9cd5dea224af231d6fe004e0b09da3d9d1eab8
Nick Waters <nicholas.r.waters@gmail.com>
2023-10-05 13:35:48 -0400

    created pseudocode steps

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Pseudocode steps for exercise solution
+
+- create string with message statement and combination (secretMessage)
+- create first number using arithmetic operator (code1)
+- create second number using arithmetic operator (code2)
+- create third number using arithmetic operator (code3)
+- create "alert" so dialogue box shows up with code
+
+*/
