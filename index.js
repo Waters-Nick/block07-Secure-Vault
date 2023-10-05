d22cb356f45c38930dea47f41fbef54f1f1e7e05
Nick Waters <nicholas.r.waters@gmail.com>
2023-10-05 13:42:36 -0400

    added pseudocode and arithmetic operations for generating secret combination.

--- index.js
+++ index.js
@@ -7,3 +7,15 @@
 - create "alert" so dialogue box shows up with code
 
 */
+
+//message string to be displayed in dialog box when user opens page.
+const secretMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+// addition operation to generate first code. Answer should be 49.
+let code1 = 12 + 33 + 4;
+//multiplication and subtraction operation to generate second code. Answer should be 26.
+let code2 = 16 * 3 - 22;
+//modular operation to generate third and final code. Answer should be
+let code3 = 103 % 24;
+
+console.log(code3);
