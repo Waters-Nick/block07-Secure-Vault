3c161e1727c1dd0b928b8617877ee53c69482117
Nick Waters <nicholas.r.waters@gmail.com>
2023-10-05 13:45:01 -0400

    added dialog box. confirmed secret combination appears as intended.

--- index.js
+++ index.js
@@ -9,13 +9,13 @@
 */
 
 //message string to be displayed in dialog box when user opens page.
-const secretMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+const secretMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 
 // addition operation to generate first code. Answer should be 49.
 let code1 = 12 + 33 + 4;
 //multiplication and subtraction operation to generate second code. Answer should be 26.
 let code2 = 16 * 3 - 22;
-//modular operation to generate third and final code. Answer should be
+//modular operation to generate third and final code. Answer should be 7.
 let code3 = 103 % 24;
 
-console.log(code3);
+alert(`${secretMessage} ${code1}-${code2}-${code3}`);
